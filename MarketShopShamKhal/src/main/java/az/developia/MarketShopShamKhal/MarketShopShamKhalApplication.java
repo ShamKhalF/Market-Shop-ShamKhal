f00923c26abba096aff96d82af2807219f4052b8
ShamKhalF <104983627+ShamKhalF@users.noreply.github.com>
2023-04-01 19:20:09 +0400

    1. create project

---
+++ MarketShopShamKhal/src/main/java/az/developia/MarketShopShamKhal/MarketShopShamKhalApplication.java
@@ -0,0 +1,13 @@
+package az.developia.MarketShopShamKhal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketShopShamKhalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketShopShamKhalApplication.class, args);
+	}
+
+}
---
+++ MarketShopShamKhal/src/test/java/az/developia/MarketShopShamKhal/MarketShopShamKhalApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.MarketShopShamKhal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketShopShamKhalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
